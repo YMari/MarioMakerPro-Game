fa9fdb8e71cfa77eba1ee84d2d76a8eed87b5e33
Yavier <Yavier@Yavier-PC>
2019-04-19 21:19:05 -0400

    + Fixed ghost animation

--- src/Game/Entities/DynamicEntities/Ghost.java
+++ src/Game/Entities/DynamicEntities/Ghost.java
@@ -7,46 +7,31 @@ import Resources.Images;
 import java.awt.*;
 
 public class Ghost extends BaseDynamicEntity {
+
+	public Ghost(int x, int y, int width, int height, Handler handler) {
+		super(x, y, width, height, handler, Images.ghost);
+	}
+
+	@Override
+	public void tick(){
+		if(!ded && dedCounter==0) {
+			super.tick();
+			if (falling) {
+				y = (int) (y + velY);
+				velY = velY + gravityAcc;
+				checkFalling();
+			}
+			checkHorizontal();
+			move();
+		}else if(ded&&dedCounter==0){
+			y++;
+			height--;
+			setDimension(new Dimension(width,height));
+			if (height==0){
+				dedCounter=1;
+				y=-10000;
+			}
+		}
+	}
 	
-	 public Ghost(int x, int y, int width, int height, Handler handler) {
-	        super(x, y, width, height, handler, Images.ghost);
-	    }
-
 }
-
-//    public Animation anim;
-//
-//    public Ghost(int x, int y, int width, int height, Handler handler) {
-//        super(x, y, width, height, handler, Images.goomba[0]);
-//        anim = new Animation(160,Images.goomba);
-//    }
-//
-//    @Override
-//    public void tick(){
-//        if(!ded && dedCounter==0) {
-//            super.tick();
-//            anim.tick();
-//            if (falling) {
-//                y = (int) (y + velY);
-//                velY = velY + gravityAcc;
-//                checkFalling();
-//            }
-//            checkHorizontal();
-//            move();
-//        }else if(ded&&dedCounter==0){
-//            y++;
-//            height--;
-//            setDimension(new Dimension(width,height));
-//            if (height==0){
-//                dedCounter=1;
-//                y=-10000;
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void kill() {
-//        sprite = Images.ghost;
-//        ded=true;
-//    }
-//}
