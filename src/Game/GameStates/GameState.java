9edb6c1cc9259e1cb9384a87091bca437f5d81f9
Yavier <Yavier@Yavier-PC>
2019-04-24 23:23:20 -0400

    + Fixed render bug for singleplayer

--- src/Game/GameStates/GameState.java
+++ src/Game/GameStates/GameState.java
@@ -28,7 +28,9 @@ public class GameState extends State {
             State.setState(handler.getGame().pauseState);
         }
         handler.getMario().tick();
-        handler.getWario().tick();
+        if (Handler.multiplayer) {
+            handler.getWario().tick();
+        }
         if(handler.getMap().getListener() != null && MapBuilder.mapDone) {
         	handler.getMap().getListener().tick();
         	handler.getMap().getHand().tick();
